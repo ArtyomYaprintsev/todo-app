1dab0f9291078df26567d9f0b39c342f381009aa
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2024-03-16 15:31:55 +0500

    Add alembic migrations system

--- server/main.py
+++ server/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
-from sqlmodel import SQLModel
 
 from server.routers import tasks
-from server.settings import ENGINE
 
-SQLModel.metadata.create_all(ENGINE)
 
 app = FastAPI()
 
