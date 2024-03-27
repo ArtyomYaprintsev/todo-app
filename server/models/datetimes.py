bd985aaf3ad0e6e10037d4a1afc89cb3d9fbba38
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2024-03-28 01:22:35 +0500

    Fix default value usage

--- server/models/datetimes.py
+++ server/models/datetimes.py
@@ -4,11 +4,11 @@ from sqlmodel import Field, SQLModel
 
 
 class CreatedAdModel(SQLModel):
-    created_at: datetime = Field(default=datetime.now())
+    created_at: datetime = Field(default_factory=datetime.now)
 
 
 class ModifiedAtModel(SQLModel):
-    modified_at: datetime = Field(default=datetime.now())
+    modified_at: datetime = Field(default_factory=datetime.now)
 
 
 class TimestampModel(CreatedAdModel, ModifiedAtModel):
