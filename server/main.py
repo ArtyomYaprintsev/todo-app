e3439017e3af30d17fc0aec15b9922e9e5e8d906
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2024-03-28 01:30:01 +0500

    Add task list router usage

--- server/main.py
+++ server/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
-from server.routers import tasks
+from server.routers import tasklists, tasks
 
 app = FastAPI()
 
 add_pagination(app)
+
+app.include_router(tasklists.router)
 app.include_router(tasks.router)
