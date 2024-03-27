a67525da21cdd26a2792cc6a81cabb816466ebbc
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2024-03-28 01:23:56 +0500

    Refactor tasks and task lists models

--- server/models/tasklists.py
+++ server/models/tasklists.py
@@ -1,23 +1,40 @@
 from typing import List, Optional
+
 from sqlmodel import Field, Relationship, SQLModel
 
 from .datetimes import TimestampModel
-from .tasks import Task
+from .tasks import Task, TaskRead
 
 
-class TaskListBase(SQLModel):
+class TaskListName(SQLModel):
+    """Model representing a task list name."""
     name: str = Field(max_length=150)
 
 
-class TaskListCreate(TaskListBase):
+class TaskListRead(TaskListName, TimestampModel):
+    """Read-only model for task lists."""
+    id: int
+
+
+class TaskList(TaskListRead, table=True):
+    """The `TaskList` model with db table relation."""
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    tasks: List[Task] = Relationship(
+        back_populates="tasklist",
+        sa_relationship_kwargs={
+            "cascade": "all,delete,delete-orphan",
+        },
+    )
+
+
+class TaskListCreate(TaskListName):
     pass
 
 
 class TaskListUpdate(SQLModel):
-    name: Optional[str]
+    name: Optional[str] = Field(max_length=150)
 
 
-class TaskList(TaskListBase, TimestampModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-
-    tasks: List[Task] = Relationship(back_populates="tasklist")
+class TaskListWithTasks(TaskListRead):
+    tasks: List[TaskRead] = []
--- server/models/tasks.py
+++ server/models/tasks.py
@@ -4,13 +4,24 @@ from sqlmodel import Field, Relationship, SQLModel
 
 from .datetimes import TimestampModel
 
-
 if TYPE_CHECKING:
-    from .tasklists import TaskList
+    from .tasklists import TaskList, TaskListRead
 
 
 class TaskBase(SQLModel):
     text: str = Field(max_length=150)
+    tasklist_id: int = Field(foreign_key="tasklist.id")
+
+
+class TaskRead(TaskBase, TimestampModel):
+    id: int
+    is_completed: bool = Field(default=False)
+
+
+class Task(TaskRead, table=True):
+    id: int = Field(default=None, primary_key=True)
+
+    tasklist: Optional["TaskList"] = Relationship(back_populates="tasks")
 
 
 class TaskCreate(TaskBase):
@@ -18,13 +29,9 @@ class TaskCreate(TaskBase):
 
 
 class TaskUpdate(SQLModel):
-    text: Optional[str]
+    text: Optional[str] = Field(max_length=150)
     is_completed: Optional[bool]
 
 
-class Task(TaskBase, TimestampModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    is_completed: bool = Field(default=False)
-
-    tasklist_id: int = Field(foreign_key="tasklist.id")
-    tasklist: Optional["TaskList"] = Relationship(back_populates="tasks")
+class TaskWithTaskList(TaskRead):
+    tasklist: Optional["TaskListRead"] = None
